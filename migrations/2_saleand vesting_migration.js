d87a0b81ab53057005a294deb9d2e3956932032e
sameep <sameepsi@gmail.com>
2018-03-19 13:41:37 +0530

    token tests

--- migrations/2_saleand vesting_migration.js
+++ migrations/2_saleand vesting_migration.js
@@ -6,7 +6,7 @@ module.exports = function(deployer) {
  deployer.deploy(DroneToken, 5000000000, "DRONE", "DRONE").then(function(){
     console.log(DroneToken.address);
     deployer.deploy(DroneTokenSale, DroneToken.address, "0xF97D9fc484024F3379D51F422F843b194E9D41C5");
-    deployer.deploy(TokenTimelock, DroneToken.address, ["0xF97D9fc484024F3379D51F422F843b194E9D41C5", "0xd78D9fc484024F3379D51F422F8432434E9D4342"], [10000, 100000], 1521444778);
+    deployer.deploy(TokenTimelock, DroneToken.address, ["0xF97D9fc484024F3379D51F422F843b194E9D41C5", "0xd78D9fc484024F3379D51F422F8432434E9D4342"], [10000, 100000], 1529481554);
 
   });
 };
